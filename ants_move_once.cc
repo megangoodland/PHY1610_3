d552ca0871903ebb17c9999db7f391b0a9521bf7
megangoodland <megangoodland@gmail.com>
2019-01-24 22:32:30 -0500

    Update ants_move_once.cc

--- ants_move_once.cc
+++ ants_move_once.cc
@@ -12,7 +12,7 @@
 #include <rarray>
 
 // ants_move_once function: have ants move into auxiliary table
-rarray<int,2> ants_move_once(const rarray<int,2>& number_of_ants, const rarray<int,2>& new_number_of_ants){
+rarray<int,2> ants_move_once(const rarray<int,2>& number_of_ants, rarray<int,2>& new_number_of_ants){
     int length = number_of_ants.extent(0);
     size_t seed       = 11;     // seed for random number generation
     const int nmoves = 9; // There are (3 in the i direction)x(3 in the j direction)=9 possible moves
@@ -22,9 +22,9 @@ rarray<int,2> ants_move_once(const rarray<int,2>& number_of_ants, const rarray<i
     for (int i = 0; i < length;i++) {
         for (int j = 0; j < length;j++) {
             int n = i*length + j; // linear index
-            if (number_of_ants[n] > 0 ) {
+            if (number_of_ants[i][j] > 0 ) {
                 // pick how many ants go in each of the 9 moves
-                rand_partition(number_of_ants[n], nmoves, partition, seed);
+                rand_partition(number_of_ants[i][j], nmoves, partition, seed);
                 // push ants in their respective moves
                 for (int m = 0; m < nmoves; m++) {
                     int i2 = i + imoves[m];
@@ -32,7 +32,7 @@ rarray<int,2> ants_move_once(const rarray<int,2>& number_of_ants, const rarray<i
                     // put only on new table if not falling off table
                     if (i2>=0 and i2<length and j2>=0 and j2<length) {
                         int n = i2*length + j2; // linear index
-                        new_number_of_ants[n] += partition[m];
+                        new_number_of_ants[i][j] += partition[m];
                     }
                 }
             }
