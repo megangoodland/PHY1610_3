6536dac80ecbd8d76a3fcf646c5eeebe35046e5a
megangoodland <megangoodland@gmail.com>
2019-01-24 21:48:21 -0500

    rarray changes

--- array_2d_tools.h
+++ array_2d_tools.h
@@ -25,20 +25,19 @@ rarray<int,2> distribute_on_array(int total, const rarray<int,2>& in_out_array);
 //      outputs: total     sum of all values in array
 //               nmin      minimum number of ants on a square
 //               nmax      maximum number of ants on a square
-std::tuple<int,int,int> count_min_max(int total, int len, const rarray<int,2>& array_2d);
+std::tuple<int,int,int> count_min_max(int total, const rarray<int,2>& array_2d);
 
 // clear_array: empty initialized 2d array
 // Parameters:
-//      input: len                  size of one dimension of array
-//             array_to_clear[]     array you want cleared
+//      input: array_to_clear[]     array you want cleared
 //      output: array_to_clear[], now filled with 0's
-rarray<int,2> clear_array(int len, const rarray<int,2>& array_to_clear);
+rarray<int,2> clear_array(const rarray<int,2>& array_to_clear);
 
 // copy array: copy and paste arrays
 // Parameters:
-//      inputs: length of one side of array, array to copy, array to paste
+//      inputs: array to copy, array to paste
 //      outputs: updated pasted array
-rarray<int,2> copy_array(int len, iconst rarray<int,2>& array_to_copy, const rarray<int,2>& array_to_paste);
+rarray<int,2> copy_array(const rarray<int,2>& array_to_copy, const rarray<int,2>& array_to_paste);
 
 
 
