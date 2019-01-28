c7abcdd1a48e3fd3415ea6f9dc60d8e7c3627dd9
megangoodland <megangoodland@gmail.com>
2019-01-27 20:50:37 -0500

    Create netCDF_writing.h

---
+++ netCDF_writing.h
@@ -0,0 +1,12 @@
+// netCDF_writing.h
+// author PHYS1610
+
+#ifndef netCDF_writing_h
+#define netCDF_writing_h
+
+// Function to write netCDF file
+//
+
+int netCDF_write();
+
+#endif /* netCDF_writing_h */
