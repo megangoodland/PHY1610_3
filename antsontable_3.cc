12e3edbda44c480f99d5bb7ff20da74ab55c609b
megangoodland <megangoodland@gmail.com>
2019-01-28 00:37:13 -0500

    Update antsontable_3.cc

--- antsontable_3.cc
+++ antsontable_3.cc
@@ -85,8 +85,7 @@ int main(){
         
         if ((t+1) % 1000 == 0){ // if the time step is divisible by 1000
             int save_number = (t+1)/1000;
-            number_of_ants_1000 = append_array(number_of_ants, number_of_ants_1000, save_number);
-        }
+            number_of_ants_1000 = append_array(number_of_ants, number_of_ants_1000, save_number);}
             
     }
    
